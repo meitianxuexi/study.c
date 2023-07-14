f850aa426c2ed70cbd2d2d070d586de220714417
meitianxuexi <139556986+meitianxuexi@users.noreply.github.com>
2023-07-14 20:42:02 +0800

    Add files via upload
    练习

---
+++ diyi.cpp
@@ -0,0 +1,318 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+
+
+
+
+
+
+
+#include <stdio.h>
+//int main()
+//{
+//	double a, b, c;
+//	scanf()
+//	printf("")
+//		if (a >= b && a >= c);
+//	printf()
+
+
+//
+//int main()
+//{
+////int age = 100;
+//if (age < 18)
+//	printf("未成年\n");
+//else if (age >= 18 && age < 28)
+//	printf("青年\n");
+//else if (age >= 28 && age < 50)
+//	printf("壮年\n");
+//else if (age >= 50 && age < 90)
+//	printf("老年\n ");
+//else
+//printf("老不死\n");
+//	int a = 0;
+//	/*int b = 2;*/
+//	if (1=a)
+//	{
+//		/*if (b = 2)*/
+//			printf("hehe\n");
+//
+//		/*\n");else
+//			printf("haha*/
+//	}
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int   b = 0;
+//	scanf("%d", &b);
+//	int num = 0;
+//
+//	if(num >= 0 && num <= 100); 
+//	b = num / 2;
+//	{if (0 == b)
+//		printf("偶数\n");
+//	else
+//		printf("奇数\n");
+//	}
+//
+//	return 0;
+//}
+//int main()
+//{
+//	int i = 1;
+//	while(i <= 100)
+//
+//	{
+//		/*if (i % 2 == 1)*/
+//			printf("%d", i);
+//		i+=2;
+//	}
+//
+//
+//
+//
+//
+//
+//
+//	return  0;
+//}
+
+//int main()
+//{
+//	int day = 0;
+//	scanf("%d", &day);
+//	switch (day)
+//	{
+//	case 1:
+//		
+//	case 2:
+//		
+//	case 3:
+//		
+//	case 4:
+//		
+//	case 5:
+//		printf("工作日\n");
+//		break;
+//
+//	case 6:
+//		
+//	case 7:
+//		printf("星期7\n");
+//		break;
+//	/*case1:
+//		printf("星期1\n");*/
+//	}
+//
+//
+//	return 0;
+//}
+
+
+int main()
+{
+	int n = 1;
+	int m = 2;
+	switch (n)
+	{
+	case 1:n++;
+	case 2:m++; n++; break;
+	case 3:
+		switch (n)
+		{
+		case 1:n++;
+			case 2：m++; n++; break;
+		}
+	case 4:m++; break;
+	default:break;
+
+
+	}
+
+
+
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
